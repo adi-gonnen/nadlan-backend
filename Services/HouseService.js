27d83e4f35d0000e34ff3b4fe71ef7bf22caf0b1
Liron Reshef Steinberger <reshef.liron@gmail.com>
2018-09-26 22:52:25 +0300

    homepage dynamic and filter

--- Services/HouseService.js
+++ Services/HouseService.js
@@ -1,15 +1,34 @@
 const ObjectId = require('mongodb').ObjectId;
 const MongoService = require('./MongoService') 
 
-function getHouses() {
-    // console.log('service back before');
+function queryHouses(city, categorystr) {
+    console.log('city', city, 'categorystr', categorystr);
     var criteria = {};
+    
+    if (city) criteria['txt.en.city'] = city; 
+    if (categorystr) {
+        var categories = categorystr.split(',');
+        console.log('categories', categories);
+        criteria.category = { $in: categories };
+    };
+
+    console.log('Criteria', criteria);
     return MongoService.connect()
-        .then(db => {
-            const collection = db.collection('house');
-            return collection.find(criteria).toArray()
-        })
+            .then(db => {
+                const collection = db.collection('house');
+                return collection.find(criteria).toArray()
+            })
 }
+
+// function getHouses() {
+//     // console.log('service back before');
+//     var criteria = {};
+//     return MongoService.connect()
+//         .then(db => {
+//             const collection = db.collection('house');
+//             return collection.find(criteria).toArray()
+//         })
+// }
 
 function getHouseById(houseId) {
     houseId = new ObjectId(houseId)
@@ -20,7 +39,20 @@ function getHouseById(houseId) {
         })
 }
 
+function getHouseByCity(city) {
+    // houseId = new ObjectId(houseId)
+    // var cityKey = txt.en.city;
+    console.log('city in service back before', city);
+    return MongoService.connect()
+        .then(db => {
+            const collection = db.collection('house');
+            return collection.findOne({ 'txt.en.city': city })
+        })
+}
+
 module.exports = {
-    getHouses,
-    getHouseById
+    // getHouses,
+    getHouseById,
+    queryHouses,
+    getHouseByCity
 }
--- routes/HouseRoute.js
+++ routes/HouseRoute.js
@@ -1,12 +1,22 @@
-const HouseService = require('../services/HouseService')
+const HouseService = require('../Services/HouseService')
 
 module.exports = (app) => {
 
     app.get('/house', (req, res) => {
-        console.log('router back');        
-        HouseService.getHouses()
-            .then(houses => res.json(houses))
+        HouseService.queryHouses(req.query.city, req.query.category)
+            .then(houses => {
+                console.log('houses in router back ', houses);   
+                res.json(houses)
+            })
+        // HouseService.queryHouses()
+        // .then(houses => res.json(houses))
     })
+
+    // app.get('/house', (req, res) => {
+    //     console.log('router back');        
+    //     HouseService.getHouses()
+    //         .then(houses => res.json(houses))
+    // })
     
     app.get('/house/:houseId', (req, res) => {
         const houseId = req.params.houseId;
@@ -14,6 +24,17 @@ module.exports = (app) => {
         HouseService.getHouseById(houseId)
         .then(house => {
             // console.log('house??', house);
+            res.json(house)
+        })
+    })
+
+    app.get('/house/gallery/:city', (req, res) => {
+        console.log('req.params in route', req.params);
+        const city = req.params.city;
+        
+        HouseService.getHouseByCity(city)
+        .then(house => {
+            console.log('house in route from back server', house);
             res.json(house)
         })
     })
